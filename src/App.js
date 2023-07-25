a474c46731c02142cda38fe5f6d54edc9523fa88
Dimitreee <danewdima@gmail.com>
2023-07-25 10:30:45 +0800

    iniitial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        let fullscreen = document.querySelector("img");
+        let button = document.querySelector("button");
+
+        button.addEventListener("click", () => {
+            if (!document.fullscreenElement) {
+                fullscreen?.requestFullscreen();
+            } else {
+                document.exitFullscreen();
+            }
+        });
+    }, [])
+
+    return (
+        <div className="container">
+            <p>
+                When the image goes to fullscreen mode to preserve its aspect ratio, it won't fill the whole screen therefore
+                a background might appear. <br/> Using :fullscreen pseudo-class selector we can control the background and
+                other styles:
+            </p>
+
+            <button>Toggle Fullscreen</button>
+
+            <img src="https://source.unsplash.com/600x400/?iran" alt="" />
+
+        </div>
+    );
 }
 
 export default App;
